72cc2b6745e713ffafe6503c3616839534fee57c
Michal Wronski <michal.robert.wronski@gmail.com>
2022-04-22 13:58:47 +0200

    Pod list info changed

--- providers/k8s/pods.go
+++ providers/k8s/pods.go
@@ -1,11 +1,21 @@
 package k8s
 
 import (
+	"time"
+
 	"github.com/bigmikesolutions/wingman/pkg/iam"
 	"github.com/pkg/errors"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
 )
+
+type PodInfo struct {
+	Name     string        `json:"name"`
+	Ready    string        `json:"ready"`
+	Status   string        `json:"status"`
+	Restarts int           `json:"restarts"`
+	Age      time.Duration `json:"age"`
+}
 
 type ResourcePods struct {
 	pods   *v1.PodList
@@ -34,5 +44,15 @@ func (p ResourcePods) Execute(action iam.ResourceAction) error {
 }
 
 func (p ResourcePods) GetInfo() interface{} {
-	return p.pods.Items
+	info := make([]PodInfo, 0)
+	for _, pod := range p.pods.Items {
+		info = append(info, PodInfo{
+			Name:     pod.Name,
+			Ready:    GetReadyInfo(&pod),
+			Status:   string(pod.Status.Phase),
+			Restarts: GetRestartCount(&pod),
+			Age:      time.Now().Sub(pod.Status.StartTime.Time),
+		})
+	}
+	return info
 }
